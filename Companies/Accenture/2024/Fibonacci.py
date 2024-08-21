268e04163972fa27630eafd367358387e661b4e8
Md Ashique Ali <mdashiqueali71@gmail.com>
2024-08-22 02:28:30 +0530

    Accenture

---
+++ Companies/Accenture/2024/Fibonacci.py
@@ -0,0 +1,12 @@
+def findFibonacci(n):
+    if n <= 0:
+        return []
+    elif n ==1:
+        return [0]
+    fib = [0,1]
+    while len(fib) < n:
+        fib.append(fib[-1] + fib[-2])
+    return fib
+
+n = int(input("Enter the number: "))
+print(findFibonacci(n))
