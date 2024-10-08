27221055c615ad43b17df47ddfa186178d3b2cdc
ashique6465 <mdashiqueali71@gmail.com>
2024-10-09 04:06:03 +0530

    dsa

---
+++ DSA/BST/BFS.py
@@ -0,0 +1,13 @@
+def BFS(self):
+    current_node = self.root
+    queue = []
+    result = []
+    queue.append(current_node)
+    while len(queue) > 0:
+        current_node = queue.pop(0)
+        result.append(current_node.value)
+        if current_node.left is not None:
+            queue.append(current_node.left)
+        if current_node.right is not None:
+            queue.append(current_node.right)
+        return result
